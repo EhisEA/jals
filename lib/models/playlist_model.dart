7025784c373eafa97fd1d2ce5991a207982a0e92
EhisEA <emmanueleayemere@gmail.com>
2021-03-26 05:37:43 +0100

    created playlist model

---
+++ lib/models/playlist_model.dart
@@ -0,0 +1,27 @@
+import 'package:jals/models/audio_model.dart';
+
+class PlayListModel {
+  final String title;
+  final int count;
+  final String id;
+  final DateTime dateUpdated;
+  final List<AudioModel> tracks;
+
+  PlayListModel({
+    this.title,
+    this.count,
+    this.id,
+    this.dateUpdated,
+    this.tracks,
+  });
+
+  factory PlayListModel.fromJson(Map<String, dynamic> json) {
+    return PlayListModel(
+        title: json["title"],
+        count: json["count"],
+        id: json["id"],
+        dateUpdated:DateTime.parse( json["date_updated"]),
+        tracks: List<AudioModel>.from(
+            json["tracks"].map((audio) => AudioModel.fromJson(audio))));
+  }
+}
